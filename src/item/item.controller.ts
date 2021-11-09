535ddbcbf316d7658762ff00141a50dc8e0540e2
Me <U@U.local>
2021-11-09 15:42:20 +0900

    first

---
+++ src/item/item.controller.ts
@@ -0,0 +1,94 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Param,
+    HttpException,
+    HttpStatus,
+  } from "@nestjs/common"; // 編集！
+  import { ItemService } from "./item.service";
+  import { Item } from "../entities/item.entity";
+  import { CreateItemDTO, UpdateItemDTO, DeleteItemDTO } from "./item.dto";
+  import { InsertResult, UpdateResult, DeleteResult } from "typeorm";
+  @Controller("item")
+  export class ItemController {
+    constructor(private readonly service: ItemService) {}
+  
+    @Get()
+    async getItemList(): Promise<Item[]> {
+      return await this.service.findAll();
+    }
+  
+    @Post()
+    async addItem(@Body() item: CreateItemDTO): Promise<InsertResult> {
+      return await this.service.create(item);
+    }
+  
+    @Get(":id")
+    async getItem(@Param("id") id: string): Promise<Item> {
+      return await this.service.find(Number(id));
+    }
+
+    @Put(":id/update")
+    async update(
+      @Param("id") id: string,
+      @Body() itemData: UpdateItemDTO,
+    ): Promise<UpdateResult> {
+      const newData = !itemData.isDone
+        ? itemData
+        : {
+          ...itemData,
+          ...{ isDone: itemData.isDone.toLowerCase() === "true" },
+        };
+      return await this.service.update(Number(id), newData);
+    }
+
+    @Delete(":id/delete")
+    async delete(@Param("id") id: string): Promise<DeleteResult> {
+    return await this.service.delete(Number(id));
+    }
+
+    @Post(":id/delete")
+    async deleteItem(
+      @Param("id") id: string,
+      @Body() deleteItem: DeleteItemDTO,
+    ) {
+      const item = await this.service.find(Number(id));
+      if (!item) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            error: `Missing item(id: ${id}).`,
+          },
+          404,
+        );
+      }
+      try {
+        await this.service.deleteByPassword(
+          Number(id),
+          deleteItem.deletePassword,
+        );
+      } catch (e) {
+        if (e.message === "Incorrect password") {
+          throw new HttpException(
+            {
+              status: HttpStatus.FORBIDDEN,
+              error: "Incorrect password",
+            },
+            403,
+          );
+        }
+        throw new HttpException(
+          {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            error: "Internal server error.",
+          },
+          500,
+        );
+      }
+      return;
+    }
+  }
